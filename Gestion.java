bfb4f242939ed0212b0536aa59f24f3888387312
ysy <yemoletrader@gmail.com>
2026-06-05 20:53:35 +0000

    première partie du travail de chef projet avant forks de l'équipe

---
+++ Gestion.java
@@ -0,0 +1,31 @@
+public class Gestion{
+
+    // Functionnalities
+    /*
+    1- ajouterProduit() ==> Jean Urbain
+    2- receptionnerProduit(int pQuantite) ==> Marie-Princesse 
+    3- vendreProduit(int pQuantite) ==> Gloria
+    4- afficherStock() ==> Fovi Alphonse
+    5- afficherRuptureStock() ==> René KingBrain
+    6- Gestion de la méthode main. ==> Me ( c'est doyi )
+    */
+
+
+    public class TeamGestion{
+        // Attributs
+        private String nomProduit="";
+        private int quantite=0;
+        private double prix=0;
+
+        // Constructeur d'initialisation
+        public TeamGestion(String pNomProduit,int pPrix){
+            nomProduit = pNomProduit;
+            prix = pPrix;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # gestion-stock
+## Fonctionnalités
+**1- ajouterProduit() ==> Jean Urbain**
+**2- receptionnerProduit(int pQuantite) ==> Marie-Princesse**
+**3- vendreProduit(int pQuantite) ==> Gloria**
+**4- afficherStock() ==> Fovi Alphonse**
+**5- afficherRuptureStock() ==> René KingBrain**
+**6- Gestion de la méthode main. ==> Me ( c'est doyi )**
